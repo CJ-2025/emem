f867885705df8c40257ca96ae0f30b4bed030c72
CJ-2025 <202280263@psu.palawan.edu.ph>
2026-03-03 13:28:00 +0800

    feat: Add Electron support for desktop app
    Integrates Electron and electron-builder to package the price tag generator as a standalone desktop application. This includes setting up the main process file, updating package.json scripts, and configuring build settings for Windows NSIS installers.

---
+++ electron-main.cjs
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    title: "EMCOR Price Tag Generator",
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+    // Remove default menu
+    autoHideMenuBar: true,
+  });
+
+  // In production, load the built index.html
+  // In development, you could load the dev server URL
+  const isDev = process.env.NODE_ENV === 'development';
+  
+  if (isDev) {
+    win.loadURL('http://localhost:3000');
+  } else {
+    win.loadFile(path.join(__dirname, 'dist/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
